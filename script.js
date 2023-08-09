0a357697d4d90d09d2541fa5fb0655fee181a2d2
Theo Roberts <theo10roberts@gmail.com>
2023-08-10 00:19:54 +0100

    change fight mechanic

--- script.js
+++ script.js
@@ -4,27 +4,35 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
-document.getElementById("beginbtn").addEventListener('click', function(){
+document.querySelector("script").addEventListener('load', function(){
     const playerhealth = document.getElementById('playerhealth')
     const computerhealth = document.getElementById('computerhealth')
     playerhealth.textContent = (playerHealth)
     computerhealth.textContent = (computerHealth)
 });
 
-document.getElementById("playerbtn").addEventListener('click', function(){
+document.getElementById("fightbtn").addEventListener('click', function(){
     playerDamage = getRandomNumber(1,10);
     computerDamage = getRandomNumber(1, 10);
+    document.querySelector('#playercharacter').classList.add('playerattack');
+    setTimeout(()=>{
+        document.querySelector('#playercharacter').classList.remove('playerattack')
+    }, 100);
     playerHealth = playerHealth - computerDamage;
     computerHealth = computerHealth - playerDamage;
-    console.log(computerHealth, playerHealth, playerDamage, computerDamage);
-    updateHealth(playerHealth, computerHealth);
-    checkWinner()
+    updateComputerHealth();
+    setTimeout(()=>{
+        document.querySelector('#computercharacter').classList.add('computerattack');
+        const playerhealth = document.getElementById('playerhealth');
+        playerhealth.textContent = (playerHealth);
+        setTimeout(()=>{
+            document.querySelector('#computercharacter').classList.remove('computerattack')
+        }, 100);
+    }, 1200);
 });
 
-function updateHealth(playerHealth, computerHealth){
-    const playerhealth = document.getElementById('playerhealth')
+function updateComputerHealth(){
     const computerhealth = document.getElementById('computerhealth')
-    playerhealth.textContent = (playerHealth)
     computerhealth.textContent = (computerHealth)
 }
 
@@ -36,3 +44,7 @@ function checkWinner(){
         alert ('you win')
     }
 }
+
+
+
+
